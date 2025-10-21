4138668fb0c2f1bdd2babe8f4ab7b184d83ead21
0xEbrahim <elsayedebrahim603@gmail.com>
2025-10-21 19:36:40 +0300

    feat: added handler client for the http requests

--- patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -1,7 +1,27 @@
 package com.pm.patientservice.controller;
 
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/patients")
 public class PatientController {
+    private final PatientService patientService;
+
+    @Autowired
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<PatientResponseDTO>> findAll() {
+        return  ResponseEntity.ok(patientService.findAll());
+    }
 }
