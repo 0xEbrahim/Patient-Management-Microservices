581d5be7e2ded8d69447662d3ab339eb66af33f0
0xEbrahim <elsayedebrahim603@gmail.com>
2025-10-21 20:17:55 +0300

    feat(patient): exception handler

---
+++ patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.pm.patientservice.exception;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) ->
+
+                errors.put(((FieldError) error).getField(), error.getDefaultMessage())
+
+        );
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
